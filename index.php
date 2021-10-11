7973ad6f69a7aa7e32be89cf0ffc0792bba53eba
Karl Morisset <karl.morisset.register@gmail.com>
2021-10-11 23:01:55 +0200

    Push du résultat

--- index.php
+++ index.php
@@ -1,4 +1,91 @@
 <?php
 
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+require_once 'src/Fighter.php';
+
+$fighter1 = new Fighter("Héraclès ✨", 20, 6);
+$fighter2 = new Fighter("Lion de Némée 🦁", 11, 13);
+$round = 1;
+
+
+// Déroulement du combat
+do{
+    echo "🕛 Round #".$round++.PHP_EOL;
+
+    fightProgress($fighter1, $fighter2, $round);
+    echo "-----------------------------".PHP_EOL;    
+    
+    fightProgress($fighter2, $fighter1, $round);
+    echo "//////////////////////////////////////////////".PHP_EOL;
+}while($fighter1->getIsAlive() &&  $fighter2->getIsAlive());
+
+
+// Affichage du résultat
+echo PHP_EOL."Résultat de ce combat de titans : ".PHP_EOL;
+echo FightResult($fighter1, $fighter2).PHP_EOL;
+
+
+/**
+ * Fonction principale effectuant le combat
+ * 
+ * @param Fighter $fighter1 : Combattant #1
+ * @param Fighter $fighter2 : Combattant #2
+ * @param int $round : Round de combat
+ * 
+ */
+function fightProgress(Fighter $fighter1, Fighter $fighter2, int $round): void
+{
+    echo "Attaquant : " .fighterProfile($fighter1).PHP_EOL;
+    echo "Adversaire : " . fighterProfile($fighter2).PHP_EOL;
+    
+    $attack = $fighter1->fight($fighter2);
+    
+    echo "{$fighter1->getName()} attaque avec une puissance de {$attack['hit']}".PHP_EOL;
+    echo "Mais {$fighter2->getName()} se défend grâce à une dextérité de {$fighter2->getDexterity()}".PHP_EOL;
+    echo "Finalement {$fighter2->getName()} subit {$attack['damages']} points de dommages".PHP_EOL;
+    
+    echo fighterLife($fighter1).PHP_EOL;
+    echo fighterLife($fighter2).PHP_EOL;
+}
+
+
+/** 
+ * Retourne l'annonce du résultat final du combat
+ * 
+ * @param Fighter $fighter1 : Combattant #1
+ * @param Fighter $fighter2 : Combattant #2
+ * 
+ * @return string
+ */
+function FightResult(Fighter $fighter1, Fighter $fighter2): string
+{
+    $winner = $fighter1->getIsAlive() ? $fighter1 : $fighter2;
+    $looser = $fighter1->getIsAlive() ? $fighter2 : $fighter1;
+    
+    return "{$looser->getName()} est mort et c'est {$winner->getName()} qui a gagné";
+}
+
+
+/** 
+ * Retourne les détails du profile d'un combattant
+ *
+ * @param Fighter $fighter : Combattant dont on doit afficher le profile
+ * 
+ * @return string
+ */
+function fighterProfile(Fighter $fighter): string
+{
+    return "{$fighter->getName()} [Puissance : {$fighter->getStrength()} / Dextérité : {$fighter->getDexterity()}]";
+}
+
+
+/** 
+ * Retourne l'annonce des points de vie restants d'un combattant
+ * 
+ * @param Fighter $fighter : Combattant dont on doit afficher les points de vie restants
+ * 
+ * @return string
+ */
+function fighterLife(Fighter $fighter): string
+{
+    return "{$fighter->getName()} a désormais {$fighter} points de vie";
+}
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,123 @@
 <?php
 
-/* Fighter class definition */
+class Fighter
+{
+    private const MAX_LIFE = 100;
+
+    private string $name;
+    private int $strength;
+    private int $dexterity;
+    private int $life;
+    private bool $isAlive = true;
+
+
+    /**
+     * Constructeur de la classe
+     * 
+     * @param string $name : Nom du combattant
+     * @param int $strength : Force du combattant
+     * @param int $dexterity : Capacité de défense du combattant
+     */
+    public function __construct(string $name, int $strength, int $dexterity)
+    {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+        $this->life = self::MAX_LIFE;
+    }
+
+
+    /** 
+     * Réalisation du combat
+     * 
+     * @param Fighter $opponent : Adversaire du combattant courant
+     * @return Array
+     */
+    public function fight(Fighter $opponent)
+    {
+        if($this->getIsAlive() &&  $opponent->getIsAlive()){
+            $hit = rand(1, $this->strength);
+            $damages = $hit - $opponent->getDexterity();
+          
+            // S'assure de ne pas avoir de valeur de dommage négative
+            $damages = $damages > 0 ? $damages : 0;
+    
+            $opponent->setLife($opponent->getLife() - $damages);
+            
+            return [
+                "hit" => $hit,
+                "damages" => $damages
+            ];
+        }
+    }
+
+
+    /** 
+     * Getter pour $name
+     */    
+    public function getName()
+    {
+        return $this->name;
+    } 
+
+
+    /**
+     * Getter pour $strength
+     */
+    public function getStrength()
+    {
+        return $this->strength;
+    }
+    
+
+    /** 
+     * Getter pour $dexterity
+     */
+    public function getDexterity(): int
+    {
+        return $this->dexterity;
+    }
+    
+
+    /** 
+     * Getter pour $isAlive
+     */
+    public function getIsAlive()
+    {   
+        return $this->isAlive;
+    }    
+
+
+    /** 
+     * Getter pour $life
+     */
+    public function getLife()
+    {
+        return $this->life;
+    }
+
+
+    /** 
+     * Setter pour $life
+     */    
+    public function setLife(int $life)
+    {
+        $this->life = $life;
+        
+        // S'assure de ne pas avoir de points de vie négatifs et 
+        // vérifie que le combattant est toujours vivant
+        if ($life <= 0 ) {
+            $this->life = 0;
+            $this->isAlive = false;
+        }
+    }
+
+
+    /**
+     * Affiche les points de vie lors d'un affichage direct de la classe
+     */
+    public function __toString()
+    {
+        return "💙 ". $this->getLife();
+    }    
+}
